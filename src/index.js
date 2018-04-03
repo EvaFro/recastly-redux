be721aad7d3b00b9f79b5844f2bd6e9c769f1c9f
EvaFro <frolli.erin@gmail.com>
2018-04-03 15:45:37 -0700

    add provider to index.js

--- src/index.js
+++ src/index.js
@@ -6,10 +6,14 @@ import YOUTUBE_API_KEY from './config/youtube.js';
 import store from './store/store.js';
 //TODO: Import the Provider component from 'react-redux' here!
 
+import { Provider } from 'react-redux';
+
 //TODO: Use the Provider component to make your store available to
 //  the rest of your app.
 
 ReactDOM.render(
-    <App API_KEY={YOUTUBE_API_KEY} searchYouTube={searchYouTube} />,
+    <Provider store={store} >
+      <App />
+    </Provider>,
   document.getElementById('app')
 );
